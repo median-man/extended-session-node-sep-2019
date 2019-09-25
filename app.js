d13fe9bc86625aba65d0ed3014eb210894457cac
John Desrosiers <john.desro@gmail.com>
2019-09-24 18:05:14 -0700

    Searches Spotify using user input

--- app.js
+++ app.js
@@ -8,26 +8,42 @@ var spotify = new Spotify({
 })
 
 // get user command
+var command = process.argv[2]
+
 // get query from user
+var userSearch = process.argv.slice(3).join(' ')
+
 // if command is lookup-song
-// then query spotify
-// print first 5 results
+if (command === 'lookup-song') {
+  // then query spotify
+  // print first 5 results
+  searchSpotifyForSong(userSearch)
 
-// TODO: what if there are no results from spotify?
+  // if command is lookup-movie
+} else if (command === 'lookup-movie') {
+  // then query omdb api
+  // print the movie title, rating, plot
+  searchOmdbApiForMovieTitle(userSearch)
+}
 
-var spotifyQuery = 'All the Small Things'
-// searches spotify
-spotify
-  .search({ type: 'track', query: spotifyQuery })
-  .then(function(response) {
-    // print the first 5 results
-    for (var i = 0; i < 5; i++) {
-      printTrack(response.tracks.items[i])
-    }
-  })
-  .catch(function(err) {
-    console.log(err)
-  })
+// Takes a string: spotifyQuery and searches spotify
+// tracks for the given string
+function searchSpotifyForSong(spotifyQuery) {
+  spotify
+    .search({ type: 'track', query: spotifyQuery })
+    .then(function(response) {
+      // print the first 5 results
+      if (response.tracks.items.length === 0) {
+        console.log('\nNo matching songs.\n')
+      }
+      for (var i = 0; i < 5 && i < response.tracks.items.length; i++) {
+        printTrack(response.tracks.items[i])
+      }
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+}
 
 // accepts a track object from spotify api and prints the info
 function printTrack(track) {
@@ -40,4 +56,11 @@ function printTrack(track) {
   console.log('Url: ' + spotifyUrl)
   console.log('Artist: ' + artist)
   console.log('\n--------------------\n')
+}
+
+function searchOmdbApiForMovieTitle(search) {
+  // TODO: need to import axios
+  // TODO: use axios to query omdb api
+  // TODO: print results from omdb api query
+  console.log('TODO: This feature will be coming soon!')
 }
